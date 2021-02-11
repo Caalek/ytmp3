00d45bc279ed21cbdbf936d8df5d267f229cec7d
unknown <adamcal@protonmail.com>
2021-02-11 21:12:41 +0100

    add mp4 conversion

--- index.js
+++ index.js
@@ -4,30 +4,40 @@ const resultDiv = document.getElementById('result-div')
 const downloadLink = document.getElementById('download-link')
 const videoTitle = document.getElementById('video-title')
 
-reloadBtn.onmousedown = () => {
-    location = location
-}
+let choice = 1
 
-convertBtn.onmousedown = () => {
-    console.log('clicked')
+document.getElementById('choice1').onclick = () => {choice = 1}
+document.getElementById('choice2').onclick = () => {choice = 2}
+
+reloadBtn.onclick = () => {location = location}
+
+convertBtn.onclick = () => {
     const link = document.getElementById('link-input').value
     const loading = document.getElementById('loading-div')
     loading.style.display = 'flex'
     resultDiv.style.display = 'none'
     downloadLink.style.display = 'inline'
     reloadBtn.style.display = 'inline'
-    postData('https://ytmp3.calek.repl.co/convert-mp3', {link: link})
+
+    let endpoint
+    if (choice === 1) {
+      endpoint = 'convert-mp3'
+    } else if (choice === 2) {
+      endpoint = 'convert-mp4'
+    }
+
+    postData(`https://ytmp3.calek.repl.co/${endpoint}`, {link: link})
       .then(data => {
           loading.style.display = 'none'
           if (data['message'] === 'error') {
             resultDiv.style.display = 'flex'
-            videoTitle.innerHTML = 'That is not a YouTube URL.'
+            videoTitle.innerHTML = 'An error occured.'
             downloadLink.style.display = 'none'
             reloadBtn.style.display = 'none'
           } else {
             resultDiv.style.display = 'flex'
             videoTitle.innerHTML = data['title']
-            downloadLink.href = `https://ytmp3.calek.repl.co/download/${data['filename']}`
+            downloadLink.href = `https://ytmp3.calek.repl.co/dl/${data['filename']}`
           }
       })
 }
